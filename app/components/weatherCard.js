5d323efcd1458d6a317bd960d10e75ecf4035337
thalliumsulfate <thalia.munroe@edu.sait.ca>
2024-12-06 11:12:29 -0700

    test commit 2

--- app/components/weatherCard.js
+++ app/components/weatherCard.js
@@ -2,6 +2,7 @@ import Image from 'next/image'
 
 export default function WeatherCard(props) {
     let text = JSON.parse(props.text);
+    let IP = JSON.parse(props.ip);
 
     let location = text.location;
     let current = text.current;
@@ -67,6 +68,7 @@ export default function WeatherCard(props) {
                     <p>Humidity: {current.humidity}%</p>
                     <p>Cloud Cover: {current.cloudcover}%</p>
                     <p>UV Index: {current.uv_index}</p>
+                    <p>IP: {IP.ip}</p>
                 </div>
                 <div className='flex flex-col items-center m-4 font-bold'>
                     <Image className='w-20 h-20' width={80} height={80} src={weatherIcon} alt='' />
--- app/weather/page.js
+++ app/weather/page.js
@@ -2,14 +2,14 @@ import WeatherCard from "@/app/components/weatherCard";
 import {GET} from "@/app/api/weatherstack/route";
 
 export default async function Page() {
-    let weather;
-
-    weather = await GET()
+    let weather = '{"request":{"type":"City","query":"Calgary, Canada","language":"en","unit":"m"},"location":{"name":"Calgary","country":"Canada","region":"Alberta","lat":"51.083","lon":"-114.083","timezone_id":"America\/Edmonton","localtime":"2024-12-04 09:03","localtime_epoch":1733302980,"utc_offset":"-7.0"},"current":{"observation_time":"04:03 PM","temperature":-5,"weather_code":260,"weather_icons":["https:\/\/cdn.worldweatheronline.com\/images\/wsymbols01_png_64\/wsymbol_0007_fog.png"],"weather_descriptions":["Freezing Fog"],"wind_speed":10,"wind_degree":151,"wind_dir":"SSE","pressure":1024,"precip":0,"humidity":93,"cloudcover":60,"feelslike":-9,"uv_index":0,"visibility":0,"is_day":"yes"}}'
+    let ipInfo = await GET();
+    console.log(ipInfo);
 
     return (
         <main className="flex grow w-dvw h-dvh gap-4">
             <div className='m-8 h-auto w-full'>
-                <WeatherCard text={weather}/>
+                <WeatherCard text={weather} ip={ipInfo}/>
             </div>
         </main>
     );
