ad9fdd0d1d2e29312b3aed61abdb62f55590c6a4
thalliumsulfate <thalia.munroe@edu.sait.ca>
2024-12-06 12:41:54 -0700

    final push

--- app/components/weatherCard.js
+++ app/components/weatherCard.js
@@ -16,10 +16,10 @@ export default function WeatherCard(props) {
         }
     }
 
-    let location = info.location;
-    let current = info.current;
-    let weatherIcon = current.weather_icons[0];
-    let description = current.weather_descriptions[0]
+    const location = info.location;
+    const current = info.current;
+    const weatherIcon = current.weather_icons[0];
+    const description = current.weather_descriptions[0]
 
     function time() {
         let timeHour = current.observation_time.substring(0,2)
@@ -59,34 +59,78 @@ export default function WeatherCard(props) {
         return timeHour + ':' + timeMinutes + ' ' + am_pm;
     }
 
-    let timeValue = time();
+    const timeValue = time();
+    const temp = Number(current.temperature);
+
+    function weatherSuggestion() {
+        console.log(temp <= 0 && temp > -20)
+            if (temp <= 0 && temp > -20) {
+                return 'Feeling pretty cold, make sure to wear a jacket';
+            }
+            else if(temp <= -20 && temp > -40) {
+                return 'Very cold out! Bundle up.';
+            }
+            else if(temp <= -40) {
+                return '...You should stay inside';
+            }
+            else if(temp > 0 && temp <= 10) {
+                return 'Pretty Chilly, wear some warm clothes';
+            }
+            else if(temp > 10 && temp <= 20) {
+                return 'Hoodie Weather';
+            }
+            else if(temp > 20 && temp <= 30) {
+                return 'Its nice out! have some fun in the sun';
+            }
+            else if (temp > 30 <= 40) {
+                return 'Really hot, perfect weather to go swimming';
+            }
+            else if(temp > 40) {
+                return 'Too hot, best to be near an AC unit';
+            }
+            else {
+                return 'Internal Error: IDK how you got this????';
+            }
+    }
+
+    const flavour = weatherSuggestion();
 
     return (
-        <div className="flex-col flex w-full h-full rounded-lg border-2 border-blue-200 bg-blue-300">
-            <div className="flex flex-row justify-center gap-2 rounded-t-lg bg-blue-400 pt-4 pb-4">
-                <h1 className='font-extrabold text-5xl bg-blue-200 rounded-3xl p-3 mt-4 self-center'>WeatherStorm</h1>
-            </div>
-            <div className='m-4 font-bold text-xl'>
-                <p>City: {location.name}, {location.region} </p>
-                <p>Country: {location.country}</p>
-                <p>Time of Observation: {timeValue}</p>
-            </div>
-            <div className='flex flex-row gap-2'>
-                <div className='m-4 font-bold text-xl'>
-                    <p>Temperature: {current.temperature} C</p>
-                    <p>Feels Like: {current.feelslike} C</p>
-                    <p>Wind Speed: {current.wind_speed} KM/H</p>
-                    <p>Wind Direction: {current.wind_dir} {current.wind_degree}°</p>
-                    <p>Humidity: {current.humidity}%</p>
-                    <p>Cloud Cover: {current.cloudcover}%</p>
-                    <p>UV Index: {current.uv_index}</p>
+        <div className="flex flex-col rounded-lg border-2 border-blue-200 bg-blue-300">
+            <div className='flex flex-row'>
+                <div className="flex flex-row items-center justify-center gap-2 rounded-l-xl bg-blue-400 p-4">
+                    <h1 className='font-extrabold text-5xl bg-blue-200 rounded-3xl p-3 mt-4'>WeatherStorm</h1>
                 </div>
-                <div className='flex flex-col items-center m-4 font-bold'>
-                    <Image className='w-20 h-20' width={80} height={80} src={weatherIcon} alt='' />
-                    <p>{description}</p>
+                <div className='flex flex-col'>
+                    <div className='flex flex-row'>
+                        <div className='m-4 font-bold text-xl'>
+                            <p>City: {location.name}, {location.region} </p>
+                            <p>Country: {location.country}</p>
+                            <p>Time of Observation: {timeValue}</p>
+                        </div>
+                        <div className='flex flex-row gap-2'>
+                            <div className='m-4 font-bold text-xl'>
+                                <p>Temperature: {temp} C</p>
+                                <p>Feels Like: {current.feelslike} C</p>
+                                <p>Wind Speed: {current.wind_speed} KM/H</p>
+                                <p>Wind Direction: {current.wind_dir} {current.wind_degree}°</p>
+                                <p>Humidity: {current.humidity}%</p>
+                                <p>Cloud Cover: {current.cloudcover}%</p>
+                                <p>UV Index: {current.uv_index}</p>
+                            </div>
+                            <div className='flex flex-col items-center m-4 font-bold'>
+                                <Image className='w-20 h-20' width={80} height={80} src={weatherIcon} alt=''/>
+                                <p>{description}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <p className='p-4'>{flavour}</p>
+                    </div>
                 </div>
 
             </div>
+
 
         </div>
     )
