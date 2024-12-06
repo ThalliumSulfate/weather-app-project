198310c0b05d0e1b9115470dc06b78b45308c1db
thalliumsulfate <thalia.munroe@edu.sait.ca>
2024-12-06 11:04:19 -0700

    removed server usage

--- app/api/weatherstack/route.js
+++ app/api/weatherstack/route.js
@@ -1,6 +1,4 @@
-'use server';
 import * as HTTP from "node:http";
-import req from "express/lib/request";
 
 const testString = '{"request":{"type":"City","query":"Calgary, Canada","language":"en","unit":"m"},"location":{"name":"Calgary","country":"Canada","region":"Alberta","lat":"51.083","lon":"-114.083","timezone_id":"America\/Edmonton","localtime":"2024-12-04 09:03","localtime_epoch":1733302980,"utc_offset":"-7.0"},"current":{"observation_time":"04:03 PM","temperature":-5,"weather_code":260,"weather_icons":["https:\/\/cdn.worldweatheronline.com\/images\/wsymbols01_png_64\/wsymbol_0007_fog.png"],"weather_descriptions":["Freezing Fog"],"wind_speed":10,"wind_degree":151,"wind_dir":"SSE","pressure":1024,"precip":0,"humidity":93,"cloudcover":60,"feelslike":-9,"uv_index":0,"visibility":0,"is_day":"yes"}}';
 
